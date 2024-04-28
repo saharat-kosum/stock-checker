fcaff578d66efb90f99054f43ca7682917fc1689
phai <pekopon_@live.fr>
2024-04-28 22:55:57 +0700

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <h1>Oops!</h1>
+      <p>Page not found</p>
+      <div>Go home</div>
+    </div>
+  );
+}
+
+export default NotFound;
